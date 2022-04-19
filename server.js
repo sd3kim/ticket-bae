08691276b5982d45c1162687e7363ffb0b96daa6
sd3kim <sd3kim@gmail.com>
2022-04-19 15:14:39 -0400

    Refactor and add backend files

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 // require("dotenv").config();
-require("./backend/config/database");
-
 const app = express();
+require("./backend/config/database");
 
 const port = process.env.PORT || 3001;
 
@@ -14,7 +13,10 @@ app.use(express.json());
 // app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-app.use("/api/ticketbae", require("./backend/routes/api/ticketbae"));
+app.use("/api/artists", require("./backend/routes/api/artists"));
+app.use("/api/locations", require("./backend/routes/api/locations"));
+app.use("/api/dates", require("./backend/routes/api/dates"));
+// app.use("/api/users", require("./backend/routes/api/users"));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
