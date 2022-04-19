367e721afad96b74fd3095539a6fa05b51c513fd
sd3kim <sd3kim@gmail.com>
2022-04-19 15:32:48 -0400

    Refactor folder names

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-import UserLogOut from "../components/UserLogOut/UserLogOut";
+import UserLogOut from "../UserLogOut/UserLogOut";
+
 function Nav() {
   return (
     <>
