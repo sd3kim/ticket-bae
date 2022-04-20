13348b2b0d45a52022f9644bf0842bb1bfb56f05
sd3kim <sd3kim@gmail.com>
2022-04-20 17:39:15 -0400

    Show all artist results

--- src/components/AllEvents/AllEvents.jsx
+++ src/components/AllEvents/AllEvents.jsx
@@ -5,34 +5,42 @@ export default function AllEvents({ userInput }) {
 
   useEffect(() => {
     async function fetchData() {
-
       const response = await fetch(
-      // `https://app.ticketmaster.com/discovery/v2/events?apikey=efomLcpWJQWbkN9TXlGgmOc98CZzZgkh&locale=*&startDateTime=2022-04-01T10:45:00Z&endDateTime=2022-04-30T10:45:00Z&countryCode=CA&segmentId=KZFzniwnSyZfZ7v7nJ`
-
-    );
+        `https://app.ticketmaster.com/discovery/v2/events?apikey=efomLcpWJQWbkN9TXlGgmOc98CZzZgkh&locale=*&startDateTime=2022-04-01T10:45:00Z&endDateTime=2022-04-30T10:45:00Z&countryCode=CA&segmentId=KZFzniwnSyZfZ7v7nJ`
+      );
 
       const showEvents = await response.json();
       const eventName = showEvents._embedded.events;
       const eventDateMapped = eventName.map((el) => el.dates);
       const eventDate = eventDateMapped.map((el) => el.start.localDate);
       console.log("event date", eventDate);
-      const eventMapped = eventName.map((el) => el.name);
+      const eventNameMapped = eventName.map((el) => el.name);
       const savedEventName = localStorage.getItem("userInput");
       const savedEventDate = localStorage.getItem("userInputDate");
-      console.log("saved event name", savedEventName);
-      console.log("saved event date", savedEventDate);
-      console.log("saved event mapped", eventMapped);
-      for (let i = 0; i < eventMapped.length; i++) {
-        if (savedEventName === undefined || savedEventName.length === 0) {
+      // console.log("saved event name", savedEventName);
+      // console.log("saved event date", savedEventDate);
+      // console.log("saved event mapped", eventNameMapped);
+      for (let i = 0; i < eventNameMapped.length; i++) {
+        // if no input for name or date is given:
+        if (savedEventName.length === 0 && savedEventDate.length === 0) {
           setEvent({ event: eventName });
-        } else if (savedEventName !== eventMapped[i]) {
+          // if the given name doesn't match
+        } else if (savedEventName !== eventNameMapped[i]) {
           setEvent({ event: "there are no events" });
+          // if the given date doesn't match
+          // code here
+          // if name AND date matches:
         } else if (
-          savedEventName == eventMapped[i] &&
-          savedEventDate == eventDate[i]
+          savedEventName === eventNameMapped[i] &&
+          savedEventDate === eventDate[i]
         ) {
-          console.log("yes it works", savedEventDate, savedEventName);
-          setEvent({ event: savedEventName, date: savedEventDate });
+          const eventFilterName = eventNameMapped.filter(
+            (el) => el.indexOf(savedEventName) > -1
+          );
+          // const eventFilterDate = eventDate.filter(
+          //   (el) => el.indexOf(savedEventDate) > -1
+          // );
+          setEvent({ event: eventFilterName, date: savedEventDate });
           return;
         }
       }
@@ -47,18 +55,9 @@ export default function AllEvents({ userInput }) {
         event.event && event.event.map((el) => <li>{el.name}</li>)
       ) : (
         <li>
-          {event.event},{event.date}
+          {event.event}, {event.date}
         </li>
       )}
-      <p>
-        {/* {event.event &&
-          event.event
-            .filter(
-              (n) => n.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-            )
-            .map((n) => <li>{n}</li>)} */}
-      </p>
-      {/* )} */}
     </div>
   );
 }
