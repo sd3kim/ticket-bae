fd28ded11e47b34eb1a821c1169073098168c8c3
zara sultan <zarasultan@zaras-MacBook-Pro.local>
2022-04-21 17:08:37 -0400

    adding save button functionality

--- src/pages/SavedShowsPage/SavedShowsPage.jsx
+++ src/pages/SavedShowsPage/SavedShowsPage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 export default function SavedShowsPage(props) {
+  let savedShows = [];
+  savedShows.push(localStorage.getItem("saved"));
   return (
     <div>
       SavedShowsPage
       <br />
       <br />
+      {/* {savedShows && savedShows.map((show) => <li value={show}>{show}</li>)} */}
+      {/* <input type="text" value={localStorage.getItem("saved")}></input> */}
+      <div>{savedShows}</div>
       <div>July 15 2022: Backstreet Boys @ Molson Ampitheatre</div>
     </div>
   );
