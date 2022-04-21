8b7b0553e99a366d4e8adcf3de3dcf6e5d451108
zara sultan <zarasultan@zaras-MacBook-Pro.local>
2022-04-21 17:12:41 -0400

    adding functionality to save show button

--- src/pages/SavedShowsPage/SavedShowsPage.jsx
+++ src/pages/SavedShowsPage/SavedShowsPage.jsx
@@ -1,25 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export default function SavedShowsPage(props) {
-  const [hello, setHello] = useState("hello");
-
-  useEffect(() => {
-    // GET the shows for a user
-  }, []);
-
-  useEffect(() => {
-    console.log("triggered", hello);
-  }, [hello]);
-
+  let savedShows = [];
+  savedShows.push(localStorage.getItem("saved"));
   return (
     <div>
       SavedShowsPage
       <br />
       <br />
+      <div>{savedShows}</div>
       <div>July 15 2022: Backstreet Boys @ Molson Ampitheatre</div>
-      <button onClick={() => setHello((prevHello) => prevHello + "!")}>
-        Click me hello
-      </button>
     </div>
   );
 }
