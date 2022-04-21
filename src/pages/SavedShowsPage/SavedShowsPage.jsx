13785154e46cb9adde88ab658bcd96f588c567d8
zara sultan <zarasultan@zaras-MacBook-Pro.local>
2022-04-21 18:19:40 -0400

    refactoring the code

--- src/pages/SavedShowsPage/SavedShowsPage.jsx
+++ src/pages/SavedShowsPage/SavedShowsPage.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function SavedShowsPage(props) {
   let savedShows = [];
   savedShows.push(localStorage.getItem("saved"));
+
   return (
     <div>
       SavedShowsPage
       <br />
       <br />
       <div>{savedShows}</div>
-      <div>July 15 2022: Backstreet Boys @ Molson Ampitheatre</div>
     </div>
   );
 }
