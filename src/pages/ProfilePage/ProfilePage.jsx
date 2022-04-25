4a1e8c5bc1d9a41d19aecc747b4328a6c28711ce
sd3kim <sd3kim@gmail.com>
2022-04-25 11:04:07 -0400

    Change styling of login and signup

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -7,7 +7,7 @@ export default function ProfilePage(props) {
       <h1>Profile Page</h1>
       <br />
       <br />
-      <img src="https://cdn-icons.flaticon.com/png/512/2829/premium/2829075.png?token=exp=1650859621~hmac=521663f9b2513c3ce4401efb58507687"></img>
+      <img src="https://i.imgur.com/cvqEXnb.png"></img>
       <UserLogOut setUserInState={props.setUserInState} user={props.user} />
     </div>
   );
