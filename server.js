8a9686d95c3e76be09ea6a7d6c9097fe4106b337
sd3kim <sd3kim@gmail.com>
2022-04-25 16:29:25 -0400

    Change endpoint

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.use(express.json());
 
 app.use(auth);
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 app.use("/api/users", require("./backend/routes/api/users"));
 app.use("/api/savedShows", require("./backend/routes/api/savedShows"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
 });
 
 app.listen(port, function () {
